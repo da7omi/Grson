121c1532e7e797ebed3738525343fb129fc77c2e
Abdullah-Alshalan <ab1alshalan@gmail.com>
2021-02-13 21:24:22 +0300

    write the comment

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'GRSON 3',
+        title: 'GRSON',
         theme: ThemeData(
           fontFamily: 'OpenSans', 
           primaryColor: kPrimaryColor, 
